db63730c07cd22931f0619d3828a497687708e5c
bilal-elchami <bilalchami@live.com>
2024-05-13 10:57:34 +0200

    Update nav bar

--- src/app/components/top-bar/top-bar.component.ts
+++ src/app/components/top-bar/top-bar.component.ts
@@ -1,16 +1,8 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-top-bar',
   templateUrl: './top-bar.component.html',
   styleUrl: './top-bar.component.css',
 })
-export class TopBarComponent {
-
-  constructor(private router: Router) {}
-
-  goToCreatePost() {
-    this.router.navigate(['add-post']);
-  }
-}
+export class TopBarComponent { }
